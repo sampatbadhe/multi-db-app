f541ac840ee5f14233a7333b6045c19a5173813d
sampatbadhe <sampat.badhe@kiprosh.com>
2022-01-05 21:27:46 +0530

    Example for disable_joins for has_one association

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ApplicationRecord
   belongs_to :user
   has_many :messages
+  has_one  :latest_message, -> { order('messages.created_at desc').limit(1) }, class_name: 'Message'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   has_many :events
   has_many :messages, through: :events, disable_joins: true
+
+  has_one :latest_event, -> { order('events.created_at desc').limit(1) }, class_name: 'Event'
+  has_one :latest_message, through: :latest_event, disable_joins: true
 end
